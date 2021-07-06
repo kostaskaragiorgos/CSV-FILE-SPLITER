eda1e4277408780f23775b03ee87efd8094f9bc2
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2021-07-06 11:02:55 +0300

    Update CsvFileSpliter.py

--- CsvFileSpliter.py
+++ CsvFileSpliter.py
@@ -1,6 +1,9 @@
 from tkinter import Menu, Button, StringVar, OptionMenu, messagebox as msg, Tk, Label
 from tkinter import simpledialog
 
+def helpmenu():
+    msg.showinfo("Help", "Split your csv files")
+
 class CsvFileSpliter():
     def __init__(self, master):
         self.master = master
@@ -19,12 +22,12 @@ class CsvFileSpliter():
         self.menu.add_cascade(label="About", menu=self.about_menu)
         
         self.help_menu = Menu(self.menu, tearoff=0)
-        self.help_menu.add_command(label="Help", accelerator='Ctrl+F1', command=self.helpmenu)
+        self.help_menu.add_command(label="Help", accelerator='Ctrl+F1', command=helpmenu)
         self.menu.add_cascade(label="Help", menu=self.help_menu)
         
         self.master.config(menu=self.menu)
         self.master.bind('<Alt-F4>', lambda event: self.exitmenu())
-        self.master.bind('<Control-F1>', lambda event: self.helpmenu())
+        self.master.bind('<Control-F1>', lambda event: helpmenu())
         self.master.bind('<Control-i>', lambda event: self.aboutmenu())
 
 
@@ -32,11 +35,11 @@ class CsvFileSpliter():
         if msg.askokcancel("Quit?", "Really quit?"):
             self.master.destroy()
     
-    def helpmenu(self):
-        pass
+
     
-    def aboutmenu(self):
+    def aboutmenu():
         pass
+
 
 def main():
     root = Tk()
