42b5017cf6cc7ba918c760f153234392e8f459bf
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2021-07-07 13:20:06 +0300

    Update CsvFileSpliter.py

--- CsvFileSpliter.py
+++ CsvFileSpliter.py
@@ -59,6 +59,7 @@ class CsvFileSpliter():
         
         self.master.config(menu=self.menu)
         self.master.bind('Control-o', lambda event: self.insertfile())
+        self.master.bind('<Control-F4>', lambda evemt: self.closefile())
         self.master.bind('<Alt-F4>', lambda event: self.exitmenu())
         self.master.bind('<Control-F1>', lambda event: helpmenu())
         self.master.bind('<Control-i>', lambda event: aboutmenu())
